c514e021581f2cd6c71384416653af730fae84fa
Jakub Sokołowski <157789383+Jaksok24@users.noreply.github.com>
2025-10-07 21:01:47 +0200

    Create Vehicle.py

---
+++ models/Vehicle.py
@@ -0,0 +1,7 @@
+class Vehicle():
+    def __init__(self, vID, registration, type, load_capacity, status):
+        self.vID = vID
+        self.registration = registration
+        self.type = type
+        self.load_capacity = load_capacity
+        self.status = status
