4a75eb4255e8607677b1a51697d8a69423d8a126
Jakub Sokołowski <157789383+Jaksok24@users.noreply.github.com>
2025-10-07 21:01:14 +0200

    Create User.py

---
+++ models/User.py
@@ -0,0 +1,7 @@
+class User():
+    def __init__(self, uID, login, username, role, password):
+        self.uID = uID
+        self.username = username
+        self.login = login
+        self.role = role
+        self.password = password
